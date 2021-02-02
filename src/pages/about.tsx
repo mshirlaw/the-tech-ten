b22d6576d0f3f0c0c5463e70a4e30a3b42cdc1b6
Matt Shirlaw <matt.shirlaw@gmail.com>
2021-02-02 20:18:36 +1100

    Create about page shell and header component

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../../components/header.tsx";
+
+export default function About(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <h1 role="heading">About</h1>
+    </>
+  );
+}
